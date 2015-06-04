0fc6d71a47f7985e013b599d1624d67d915d6db6
Noah Lorang <lorang@gmail.com>
2015-06-04 11:03:24 -0400

    Require poise

--- libraries/rundeck.rb
+++ libraries/rundeck.rb
@@ -17,7 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+require 'poise'
 class Chef
   class Resource::Rundeck < Resource
     include Poise(container: true)
